ed082f6c6d119c475382a3817894ed42d553ad41
Harmanpreet kaur <harmanpawar77@gmail.com>
2024-03-21 16:25:31 -0400

    Local and Global scopes in depth

---
+++ basics/scope.js
@@ -0,0 +1,59 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "hit"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "harman"
+    if (username === "harman") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+//addTwo(5) wont get excecutd as , varble cnt be  undefined
+const addTwo = function(num){
+    return num + 2
+}
+addTwo(1)
