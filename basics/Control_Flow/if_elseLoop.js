a26549d28770e17218b65cf84b4a4aa4c51e361c
Harmanpreet kaur <harmanpawar77@gmail.com>
2024-03-27 12:37:25 -0400

    if_else Loop

--- basics/Control_Flow/if_elseLoop.js
+++ basics/Control_Flow/if_elseLoop.js
@@ -22,3 +22,50 @@ else {
     console.log(`Hey ${person1} , lets go out for trekkiing`)
 }
 
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+// nesting in if else loop with multiple conditions:
+
+let userLoggedIn=true;
+let debitCardDetails=true;
+let googleLoggedIn=true;
+let emailLogged=false;
+
+if (userLoggedIn && debitCardDetails) {
+    console.log("Welcome User, you have passesed authtentication")
+}
+if (googleLoggedIn || emailLogged)
+{
+    console.log(`You are sucessfully logged INN`)
+}
+else {
+    console.log(`Please login through Google or Email `)
+    
+}
+
